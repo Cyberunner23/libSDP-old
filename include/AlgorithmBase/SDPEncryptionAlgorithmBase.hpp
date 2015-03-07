00ce86eaf43ded0e4b2da8c5e32bd5c2849d800f
Cyberunner23 <narutoxela@gmail.com>
2015-03-06 23:21:57 -0500

    Missed some types in the uint style conversion.

--- include/AlgorithmBase/SDPEncryptionAlgorithmBase.hpp
+++ include/AlgorithmBase/SDPEncryptionAlgorithmBase.hpp
@@ -38,12 +38,12 @@ public:
     ~SDPEncryptionAlgorithmBase();
 
 	//These functions will only be used if isStreamCipher = false
-    virtual uint_least64_t decrypt(unsigned char* encryptedBuffer,   unsigned char* decryptedBuffer, uint_least64_t encryptedBufferSize,   uint_least64_t chunkNum) = 0;
-    virtual uint_least64_t encrypt(unsigned char* unencryptedBuffer, unsigned char* encryptedBuffer, uint_least64_t unencryptedBufferSize, uint_least64_t chunkNum) = 0;
+    virtual uint_least64_t decrypt(unsigned char* encryptedBuffer,   unsigned char* decryptedBuffer, uint64 encryptedBufferSize,   uint64 chunkNum) = 0;
+    virtual uint_least64_t encrypt(unsigned char* unencryptedBuffer, unsigned char* encryptedBuffer, uint64 unencryptedBufferSize, uint64 chunkNum) = 0;
 
 	//These functions will only be used if isStreamCipher = true
-	virtual bool decryptStream(unsigned char *encryptedChar,   unsigned char *unencryptedChar, uint_least64_t charNum) = 0;
-	virtual bool encryptStream(unsigned char *unencryptedChar, unsigned char *encryptedChar,   uint_least64_t charNum) = 0;
+	virtual bool decryptStream(unsigned char *encryptedChar,   unsigned char *unencryptedChar, uint64 charNum) = 0;
+	virtual bool encryptStream(unsigned char *unencryptedChar, unsigned char *encryptedChar,   uint64 charNum) = 0;
 
 	virtual void onInit() = 0;
 	virtual void onExit() = 0;
