310fe457a9cd41b52878feff7362704f9746d62a
Cyberunner23 <narutoxela@gmail.com>
2015-03-20 22:47:40 -0400

    After months of procrastinating... Copyright year has been updated.

--- src/SDPCryptStreamBuf.cpp
+++ src/SDPCryptStreamBuf.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright 2014 Alex Frappier Lachapelle
+Copyright 2015 Alex Frappier Lachapelle
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
