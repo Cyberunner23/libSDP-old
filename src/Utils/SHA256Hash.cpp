41d7a751c52b9cd125f6a55fc316b23730c4464d
Cyberunner23 <narutoxela@gmail.com>
2015-08-23 07:25:46 -0400

    static_cast -> dynamic_cast for certain casts in the sha256 hashing util. (GCC was again compiling fine with invalid usage of casts...)

--- src/Utils/SHA256Hash.cpp
+++ src/Utils/SHA256Hash.cpp
@@ -78,34 +78,34 @@ void SHA256Hash::updateHash(uchar* val, uint64 size, Args... args){
 }
 
 template<typename... Args>
-void SHA256Hash::updateHash(uint16 val, Args... args){
+void SHA256Hash::updateHash(uint16 val, Args... args){////////////////////////
     crypto_hash_sha256_update(&sha256State,
-                              static_cast<uchar*>(val),
-                              sizeof(static_cast<uchar*>(val)));
+                              dynamic_cast<uchar*>(val),
+                              sizeof(dynamic_cast<uchar*>(val)));
     updateHash(args...);
 }
 
 template<typename... Args>
-void SHA256Hash::updateHash(uint32 val, Args... args){
+void SHA256Hash::updateHash(uint32 val, Args... args){/////////////////////
     crypto_hash_sha256_update(&sha256State,
-                              static_cast<uchar*>(val),
-                              sizeof(static_cast<uchar*>(val)));
+                              dynamic_cast<uchar*>(val),
+                              sizeof(dynamic_cast<uchar*>(val)));
     updateHash(args...);
 }
 
 template<typename... Args>
-void SHA256Hash::updateHash(uint64 val, Args... args){
+void SHA256Hash::updateHash(uint64 val, Args... args){/////////////////////////
     crypto_hash_sha256_update(&sha256State,
-                              static_cast<uchar*>(val),
-                              sizeof(static_cast<uchar*>(val)));
+                              dynamic_cast<uchar*>(val),
+                              sizeof(dynamic_cast<uchar*>(val)));
     updateHash(args...);
 }
 
 template<typename... Args>
-void SHA256Hash::updateHash(std::string val, Args... args){
+void SHA256Hash::updateHash(std::string val, Args... args){///////////////////
     crypto_hash_sha256_update(&sha256State,
-                              static_cast<const uchar*>(val.c_str()),
-                              sizeof(static_cast<const uchar*>(val.c_str())));
+                              dynamic_cast<const uchar*>(val.c_str()),
+                              sizeof(dynamic_cast<const uchar*>(val.c_str())));
     updateHash(args...);
 }
 
@@ -118,26 +118,26 @@ void SHA256Hash::updateHash(std::vector<uchar> val, Args... args){
 }
 
 template<typename... Args>
-void SHA256Hash::updateHash(std::vector<uint16> val, Args... args){
+void SHA256Hash::updateHash(std::vector<uint16> val, Args... args){//////////////////
     crypto_hash_sha256_update(&sha256State,
-                              static_cast<uchar*>(val.data()),
-                              sizeof(static_cast<uchar*>(val.data())));
+                              dynamic_cast<uchar*>(val.data()),
+                              sizeof(dynamic_cast<uchar*>(val.data())));
     updateHash(args...);
 }
 
 template<typename... Args>
-void SHA256Hash::updateHash(std::vector<uint32> val, Args... args){
+void SHA256Hash::updateHash(std::vector<uint32> val, Args... args){///////////////////////
     crypto_hash_sha256_update(&sha256State,
-                              static_cast<uchar*>(val.data()),
-                              sizeof(static_cast<uchar*>(val.data())));
+                              dynamic_cast<uchar*>(val.data()),
+                              sizeof(dynamic_cast<uchar*>(val.data())));
     updateHash(args...);
 }
 
 template<typename... Args>
-void SHA256Hash::updateHash(std::vector<uint64> val, Args... args){
+void SHA256Hash::updateHash(std::vector<uint64> val, Args... args){///////////////////////
     crypto_hash_sha256_update(&sha256State,
-                              static_cast<uchar*>(val.data()),
-                              sizeof(static_cast<uchar*>(val.data())));
+                              dynamic_cast<uchar*>(val.data()),
+                              sizeof(dynamic_cast<uchar*>(val.data())));
     updateHash(args...);
 }
 
