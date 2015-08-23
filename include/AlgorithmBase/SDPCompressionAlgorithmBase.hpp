0685e38fa7ba98943ae12eed195cce0247117fe7
Cyberunner23 <narutoxela@gmail.com>
2015-08-22 20:51:40 -0400

    Apparently these two files we not included in the last commit...

--- include/AlgorithmBase/SDPCompressionAlgorithmBase.hpp
+++ include/AlgorithmBase/SDPCompressionAlgorithmBase.hpp
@@ -19,73 +19,87 @@ Copyright 2015 Alex Frappier Lachapelle
 
 #include "Typedefs.hpp"
 
-class SDPCompressionAlgorithmBase{
+namespace libSDP{
+namespace AlgorithmBase{
 
-public:
+    using namespace libSDP::Utils;
 
-    //vars
+    class SDPCompressionAlgorithmBase{
 
-    //funcs
+    public:
 
-    SDPCompressionAlgorithmBase();
-    ~SDPCompressionAlgorithmBase();
+        //vars
 
-    virtual uint64 decompressBuffer(uchar* compressedBuffer,   uchar* decompressedBuffer, uint64 compressedBufferSize,   uint64 decompressedDataSize,  uint64 chunkNum) = 0;
-    virtual uint64 compressBuffer(uchar*   uncompressedBuffer, uchar* compressedBuffer,   uint64 uncompressedBufferSize, uint64 chunkNum) = 0;
+        //funcs
 
-    virtual void onInit() = 0;
-    virtual void onExit() = 0;
-    virtual void onSync() = 0;
+        SDPCompressionAlgorithmBase();
 
-	uint64 getBufferSize();
-	uint64 getBufferSizeWithOverhead();
+        ~SDPCompressionAlgorithmBase();
 
+        virtual uint64 decompressBuffer(uchar *compressedBuffer, uchar *decompressedBuffer, uint64 compressedBufferSize,
+                                        uint64 decompressedDataSize, uint64 chunkNum)=0;
 
-protected:
+        virtual uint64 compressBuffer(uchar *uncompressedBuffer, uchar *compressedBuffer, uint64 uncompressedBufferSize,
+                                      uint64 chunkNum)=0;
 
-	//Vars
+        virtual void onInit()=0;
 
-	//Funcs
+        virtual void onExit()=0;
 
-	//Should be set somewhere in the algorithm's constructor
-	//Sets the maximum buffer size supported by the algorithm.
-	//Used when isStreamCompression = false
-	void setMaxBufferSize(uint64 maxBufferSize);
+        virtual void onSync()=0;
 
-	//Should be set somewhere in the algorithm's constructor after setIsStreamCompression(true/false)
-	//and setMaxBufferSize(uint64)
-	//Sets the buffer size to be used when isStreamCipher = false (block compression)
-	bool setPreferedBufferSize(uint64 preferedBufferSize);
+        uint64 getBufferSize();
 
-    //Should be set somewhere in the algorithm's constructor after setIsStreamCompression(true/false)
-    //and setMaxBufferSize(uint64)
-	//Buffer size including possible overhead on compression to be used when isStreamCompression = false
-	//This is in case the data is incompressible and that the algorithm introduces an overhead.
-	//This is to prevent an overflow and a SIGSEGV.
-	bool setPreferedBufferSizeWithOverhead(uint64 preferedBufferSizeWithOverhead);
+        uint64 getBufferSizeWithOverhead();
 
 
-private:
+    protected:
 
-    //vars
+        //Vars
 
-	//Maximum buffer size supported by the algorithm, used when isStreamCompression = false
-	//Used when isStreamCompression = false
-	uint64 maxBufferSize;
+        //Funcs
 
-	//Buffer size that will be used when isStreamCompression = false
-	//This may be smaller than the prefered buffer size if it is set to be larger than
-	//what the algorithm can handle.
-	uint64 bufferSize = 0;
+        //Should be set somewhere in the algorithm's constructor
+        //Sets the maximum buffer size supported by the algorithm.
+        //Used when isStreamCompression = false
+        void setMaxBufferSize(uint64 maxBufferSize);
 
-	//Buffer size including possible overhead on compression to be used when isStreamCompression = false
-	//This is in case the data is incompressible and that the algorithm introduces an overhead.
-	//This is to prevent an overflow and a SIGSEGV.
-	uint64 bufferSizeWithOverhead = 0;
+        //Should be set somewhere in the algorithm's constructor after setIsStreamCompression(true/false)
+        //and setMaxBufferSize(uint64)
+        //Sets the buffer size to be used when isStreamCipher = false (block compression)
+        bool setPreferedBufferSize(uint64 preferedBufferSize);
 
-    //funcs
+        //Should be set somewhere in the algorithm's constructor after setIsStreamCompression(true/false)
+        //and setMaxBufferSize(uint64)
+        //Buffer size including possible overhead on compression to be used when isStreamCompression = false
+        //This is in case the data is incompressible and that the algorithm introduces an overhead.
+        //This is to prevent an overflow and a SIGSEGV.
+        bool setPreferedBufferSizeWithOverhead(uint64 preferedBufferSizeWithOverhead);
 
 
-};
+    private:
+
+        //vars
+
+        //Maximum buffer size supported by the algorithm, used when isStreamCompression = false
+        //Used when isStreamCompression = false
+        uint64 maxBufferSize;
+
+        //Buffer size that will be used when isStreamCompression = false
+        //This may be smaller than the prefered buffer size if it is set to be larger than
+        //what the algorithm can handle.
+        uint64 bufferSize=0;
+
+        //Buffer size including possible overhead on compression to be used when isStreamCompression = false
+        //This is in case the data is incompressible and that the algorithm introduces an overhead.
+        //This is to prevent an overflow and a SIGSEGV.
+        uint64 bufferSizeWithOverhead=0;
+
+        //funcs
+
+
+    };
+}
+}
 
 #endif // SDPCOMPRESSIONALGORITHMBASE_H
--- include/AlgorithmBase/SDPEncryptionAlgorithmBase.hpp
+++ include/AlgorithmBase/SDPEncryptionAlgorithmBase.hpp
@@ -29,107 +29,127 @@ Copyright 2015 Alex Frappier Lachapelle
 //TODO: Implement seeking.
 //TODO: ability to override buffer size.
 
-class SDPEncryptionAlgorithmBase{
+namespace libSDP{
+namespace AlgorithmBase{
+
+    using namespace libSDP::Utils;
+
+    class SDPEncryptionAlgorithmBase{
 
 
-public:
+    public:
 
-    //vars
+        //vars
 
-    //funcs
+        //funcs
 
-    SDPEncryptionAlgorithmBase();
-    ~SDPEncryptionAlgorithmBase();
+        SDPEncryptionAlgorithmBase();
 
-    //These functions will only be used if isStreamCompression = false
-    virtual uint64 decryptBuffer(uchar* encryptedBuffer,   uchar* decryptedBuffer, uint64 encryptedBufferSize,   uint64 chunkNum) = 0;
-    virtual uint64 encryptBuffer(uchar* unencryptedBuffer, uchar* encryptedBuffer, uint64 unencryptedBufferSize, uint64 chunkNum) = 0;
+        ~SDPEncryptionAlgorithmBase();
 
-    //These functions will only be used if isStreamCompression = true
-    virtual bool decryptStream(uchar encryptedChar,   uchar* decryptedChar, uint64 charNum) = 0;
-    virtual bool encryptStream(uchar unencryptedChar, uchar* encryptedChar, uint64 charNum) = 0;
+        //These functions will only be used if isStreamCompression = false
+        virtual uint64 decryptBuffer(uchar *encryptedBuffer, uchar *decryptedBuffer, uint64 encryptedBufferSize,
+                                     uint64 chunkNum)=0;
 
-    virtual void onInit() = 0;
-    virtual void onExit() = 0;
-    virtual void onSync() = 0;
+        virtual uint64 encryptBuffer(uchar *unencryptedBuffer, uchar *encryptedBuffer, uint64 unencryptedBufferSize,
+                                     uint64 chunkNum)=0;
 
-    virtual int setEncryptionKeyAndNonce(std::string encryptionKey, bool isEncryptionKeyInHex, std::string nonce, bool isNonceInHex);
-    virtual int setEncryptionKey(std::string encryptionKey,         bool isEncryptionKeyInHex);
-    virtual int setNonce(std::string nonce,                         bool isNonceInHex);
+        //These functions will only be used if isStreamCompression = true
+        virtual bool decryptStream(uchar encryptedChar, uchar *decryptedChar, uint64 charNum)=0;
 
-    uint8  getEncryptionAlgorithmID();
-    bool   getIsStreamCipher();
-    uint64 getBufferSize();
-    uint64 getBufferSizeWithOverhead();
+        virtual bool encryptStream(uchar unencryptedChar, uchar *encryptedChar, uint64 charNum)=0;
 
-protected:
+        virtual void onInit()=0;
 
-    //vars
-    std::string encryptionKeyInBin;
-    std::string nonceInBin;
+        virtual void onExit()=0;
 
-    //funcs
+        virtual void onSync()=0;
 
-    //Should be set somewhere in the algorithm's constructor or onInit?
-    void setEncryptionAlgorithmID(uint8 algorithmID);
+        virtual int setEncryptionKeyAndNonce(std::string encryptionKey, bool isEncryptionKeyInHex, std::string nonce,
+                                             bool isNonceInHex);
 
-    //Should be set somewhere in the algorithm's constructor or onInit?
-    //true = stream cipher, false = block cipher.
-    void setIsStreamCipher(bool isStreamCipher);
+        virtual int setEncryptionKey(std::string encryptionKey, bool isEncryptionKeyInHex);
 
-    //Should be set somewhere in the algorithm's constructor or onInit
-    //Sets the supported key length in bits for the algorithm.
-    void setEncryptionKeyWidthInBits(unsigned int encryptionKeyWidthInBits);
+        virtual int setNonce(std::string nonce, bool isNonceInHex);
 
-    //Should be set somewhere in the algorithm's constructor or onInit
-    //Sets the supported nonce length in bits for the algorithm.
-    void setNonceWidthInBits(unsigned int nonceWidthInBits);
+        uint8 getEncryptionAlgorithmID();
 
-    //Should be set somewhere in the algorithm's constructor or onInit? after setIsStreamCipher(true/false)
-    //Sets the maximum buffer size supported by the algorithm.
-    //Used when isStreamCipher = false
-    void setMaxBufferSize(uint64 maxBufferSize);
+        bool getIsStreamCipher();
 
-    //Should be set somewhere in the algorithm's constructor or onInit? after setIsStreamCipher(true/false)
-    //and setMaxBufferSize(uint_least64_t)
-    //Sets the buffer size to be used when isStreamCipher = false (block cipher)
-    bool setPreferedBufferSize(uint64 preferedBufferSize);
+        uint64 getBufferSize();
 
-    //Sets the buffer size including possible overhead. To be used when isStreamCipher = false
-    //This is in case the algorithm adds extra data on encryption like authentication tags.
-    //This is to prevent an overflow and a SIGSEGV.
-    bool setPreferedBufferSizeWithOverhead(uint64 preferedBufferSizeWithOverhead);
+        uint64 getBufferSizeWithOverhead();
 
+    protected:
 
-private:
+        //vars
+        std::string encryptionKeyInBin;
+        std::string nonceInBin;
 
-    //vars
+        //funcs
 
-    uint8 encryptionAlgorithmID;
+        //Should be set somewhere in the algorithm's constructor or onInit?
+        void setEncryptionAlgorithmID(uint8 algorithmID);
 
-    //true = stream cipher, false = block cipher
-    bool isStreamCipher;
+        //Should be set somewhere in the algorithm's constructor or onInit?
+        //true = stream cipher, false = block cipher.
+        void setIsStreamCipher(bool isStreamCipher);
 
-    uint encryptionKeyWidthInBits;
-    uint nonceWidthInBits;
+        //Should be set somewhere in the algorithm's constructor or onInit
+        //Sets the supported key length in bits for the algorithm.
+        void setEncryptionKeyWidthInBits(unsigned int encryptionKeyWidthInBits);
 
-    //Maximum buffer size supported by the algorithm.
-    //Used when isStreamCipher = false
-    uint64 maxBufferSize;
+        //Should be set somewhere in the algorithm's constructor or onInit
+        //Sets the supported nonce length in bits for the algorithm.
+        void setNonceWidthInBits(unsigned int nonceWidthInBits);
 
-    //Buffer size to be used when isStreamCipher = false (block cipher)
-    //This may be smaller than the prefered buffer size if
-    //it is set to be larger than what the algorithm can handle.
-    uint64 bufferSize = 0;
+        //Should be set somewhere in the algorithm's constructor or onInit? after setIsStreamCipher(true/false)
+        //Sets the maximum buffer size supported by the algorithm.
+        //Used when isStreamCipher = false
+        void setMaxBufferSize(uint64 maxBufferSize);
 
-    //Buffer size including possible overhead. To be used when isStreamCipher = false
-    //This is in case the algorithm adds extra data on encryption like authentication tags.
-    //This is to prevent an overflow and a SIGSEGV.
-    uint64 bufferSizeWithOverhead = 0;
+        //Should be set somewhere in the algorithm's constructor or onInit? after setIsStreamCipher(true/false)
+        //and setMaxBufferSize(uint_least64_t)
+        //Sets the buffer size to be used when isStreamCipher = false (block cipher)
+        bool setPreferedBufferSize(uint64 preferedBufferSize);
 
-    //funcs
+        //Sets the buffer size including possible overhead. To be used when isStreamCipher = false
+        //This is in case the algorithm adds extra data on encryption like authentication tags.
+        //This is to prevent an overflow and a SIGSEGV.
+        bool setPreferedBufferSizeWithOverhead(uint64 preferedBufferSizeWithOverhead);
 
 
-};
+    private:
+
+        //vars
+
+        uint8 encryptionAlgorithmID;
+
+        //true = stream cipher, false = block cipher
+        bool isStreamCipher;
+
+        uint encryptionKeyWidthInBits;
+        uint nonceWidthInBits;
+
+        //Maximum buffer size supported by the algorithm.
+        //Used when isStreamCipher = false
+        uint64 maxBufferSize;
+
+        //Buffer size to be used when isStreamCipher = false (block cipher)
+        //This may be smaller than the prefered buffer size if
+        //it is set to be larger than what the algorithm can handle.
+        uint64 bufferSize=0;
+
+        //Buffer size including possible overhead. To be used when isStreamCipher = false
+        //This is in case the algorithm adds extra data on encryption like authentication tags.
+        //This is to prevent an overflow and a SIGSEGV.
+        uint64 bufferSizeWithOverhead=0;
+
+        //funcs
+
+
+    };
+}
+}
 
 #endif // SDPENCRYPTIONALGORITHMBASE_H
